0d865fb5046f0286526c2b6aff7f1e47a6a22e59
prockallsyms <proc@kallsyms>
2025-05-27 00:28:39 -0400

    enable keybindings for treesitter AST motions

--- .config/nvim/lua/prockallsyms/lazy/treesitter.lua
+++ .config/nvim/lua/prockallsyms/lazy/treesitter.lua
@@ -1,27 +1,63 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	lazy = false,
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			ensure_installed = {
-				"c",
-				"go",
-				"rust",
-				"toml",
-				"diff",
-				"lua",
-				"python",
-				"markdown",
-			},
-			sync_install = false,
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			}
-		})
-	end
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		lazy = false,
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+			configs.setup({
+				ensure_installed = {
+					"c",
+					"go",
+					"rust",
+					"toml",
+					"diff",
+					"lua",
+					"python",
+					"markdown",
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				}
+			})
+		end
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		lazy = false,
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["]m"] = { query = "@function.outer", desc = "Next function start" },
+							["]c"] = { query = "@class.outer", desc = "Next class start" },
+							["]o"] = "@loop.*",
+						},
+						goto_next_end = {
+							["]M"] = { query = "@function.outer", desc = "Next function end" },
+							["]C"] = { query = "@class.outer", desc = "Next class end" },
+							["]O"] = "@loop.*",
+						},
+						goto_previous_start = {
+							["[m"] = { query = "@function.outer", desc = "Previous function start" },
+							["[c"] = { query = "@class.outer", desc = "Previous class start" },
+							["[o"] = "@loop.*",
+						},
+						goto_previous_end = {
+							["[M"] = { query = "@function.outer", desc = "Previous function end" },
+							["[C"] = { query = "@class.outer", desc = "Previous class end" },
+							["[O"] = "@loop.*",
+						},
+					},
+				},
+			})
+		end
+	}
 }
