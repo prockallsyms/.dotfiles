a777012ec526061da3cfbd16bb87b102b5b0cbf0
prockallsyms <proc@kallsyms>
2025-05-29 01:28:57 -0400

    improve telescope config

--- .config/nvim/lua/prockallsyms/lazy/telescope.lua
+++ .config/nvim/lua/prockallsyms/lazy/telescope.lua
@@ -1,7 +1,25 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
+		"BurntSushi/ripgrep",
+		"sharkdp/fd",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build ="make", },
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-devicons/nvim-web-devicons",
 		"nvim-lua/plenary.nvim",
 	},
-	lazy = false
+	lazy = false,
+	opts = {
+		extensions = {
+			fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+			},
+		},
+	},
+	config = function(_, opts)
+		require("telescope").load_extension("fzf")
+	end,
 }
