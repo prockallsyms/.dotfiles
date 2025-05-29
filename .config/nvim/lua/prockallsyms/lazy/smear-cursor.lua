ba90b91750cea4336e04eda2492c4c11b1021645
prockallsyms <proc@kallsyms>
2025-05-29 02:32:58 -0400

    use temp workaround to neovim nightly issue preventing LSP code_actions

--- .config/nvim/lua/prockallsyms/lazy/smear-cursor.lua
+++ .config/nvim/lua/prockallsyms/lazy/smear-cursor.lua
@@ -7,5 +7,6 @@ return {
     scroll_buffer_space = true,
     legacy_computing_symbols_support = false,
     smear_insert_mode = true,
+    smear_to_cmd = false, -- https://github.com/sphamba/smear-cursor.nvim/issues/82
   },
 }
