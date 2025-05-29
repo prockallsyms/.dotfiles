b41a2825e847c252f129545d6c0194455ed36a98
prockallsyms <proc@kallsyms>
2025-05-29 01:27:28 -0400

    consolidate remaps to correct file

--- .config/nvim/lua/prockallsyms/remap.lua
+++ .config/nvim/lua/prockallsyms/remap.lua
@@ -14,20 +14,54 @@ vim.keymap.set("n", "<Leader>nn", builtin.builtin, { desc = "Telescope test mapp
 
 -- file finding remaps
 vim.keymap.set("n", "<Leader>ff", builtin.find_files, { desc = "[F]ind [F]ile with from root" })
-vim.keymap.set("n", "<Leader>fg", builtin.git_files, { desc = "[F]ind non-i[G]nored file from root" })
+vim.keymap.set("n", "<Leader>fg", builtin.git_files, { desc = "[F]ind non-i[G]nored file from git root" })
+vim.keymap.set("n", "<Leader>lg", builtin.live_grep, { desc = "[L]ive [G]rep files from git root" })
+
+-- buffer remaps
+vim.keymap.set("n", "<Leader>bb", builtin.buffers, { desc = "[B]uiltin [B]uffers, enumerates open buffers" })
+vim.keymap.set("n", "<Leader>bc", function() vim.cmd('bd') end, { desc = "[B]uffer [C]lose" })
+vim.keymap.set("n", "<Leader><Leader>", function() vim.cmd('b#') end, { desc = "Swap to last open buffer" })
 
 -- LSP remaps
-vim.keymap.set("n", "<Leader>lr", builtin.lsp_references, { desc = "[L]SP [R]eferences to symbol" })
-vim.keymap.set("n", "<Leader>ls", builtin.lsp_document_symbols, { desc = "[L]SP [S]ymbols in current buffer" })
-vim.keymap.set("n", "<Leader>li", builtin.lsp_implementations, { desc = "[L]SP [I]mplementation of symbol" })
-vim.keymap.set("n", "<Leader>ld", builtin.lsp_definitions, { desc = "[L]SP [D]efinition of symbol" })
-vim.keymap.set("n", "<Leader>lt", builtin.lsp_type_definitions, { desc = "[L]SP [T]ype definitions of symbol" })
+autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(args)
+	vim.bo[args.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+	local opts = { buffer = args.buf }
+	-- lsp keymaps
+	vim.keymap.set("n", "<Leader>lr", builtin.lsp_references, { desc = "[L]SP [R]eferences to symbol" })
+	vim.keymap.set("n", "<Leader>ls", builtin.lsp_document_symbols, { desc = "[L]SP [S]ymbols in current buffer" })
+	vim.keymap.set("n", "<Leader>li", builtin.lsp_implementations, { desc = "[L]SP [I]mplementation of symbol" })
+	vim.keymap.set("n", "<Leader>ld", builtin.lsp_definitions, { desc = "[L]SP [D]efinition of symbol" })
+	vim.keymap.set("n", "<Leader>lt", builtin.lsp_type_definitions, { desc = "[L]SP [T]ype definitions of symbol" })
+	vim.keymap.set("n", "<Leader>bf", function() vim.lsp.buf.format({ timeout = 5000 }) end, { desc = "[B]uffer [F]ormat the current buffer" })
+	vim.keymap.set("n", "<Leader>e", vim.diagnostic.open_float, { desc = "[E]rror [D]iagnostic" })
+	vim.keymap.set("n", "<Leader>dl", vim.diagnostic.setloclist, { desc = "[D]iagnostic [L]ist" })
+
+	opts.desc = "LSP [H]over"
+	vim.keymap.set("n", "<Leader>h", vim.lsp.buf.hover, opts)
+
+	opts.desc = "LSP [C]ode [A]ction"
+	vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, opts)
+
+	opts.desc = "[W]orkspace [A]dd folder"
+	vim.keymap.set("n", "<Leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+
+	opts.desc = "[W]orkspace [R]emove folder"
+	vim.keymap.set("n", "<Leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+
+	opts.desc = "List [W]orkspace [F]olders"
+	vim.keymap.set("n", "<Leader>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+	if vim.lsp.inlay_hint then
+	    vim.lsp.inlay_hint.enable(true, opts)
+	end
+    end,
+})
 
 -- git remaps
 vim.keymap.set("n", "<Leader>gs", builtin.git_status, { desc = "[G]it [S]tatus" })
-vim.keymap.set("n", "<Leader>gb", builtin.git_branches, { desc = "[G]it [B]ranches" })
+vim.keymap.set("n", "<Leader>gbr", builtin.git_branches, { desc = "[G]it [B]ranches" })
 vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Use Fugitive Git" })
-
 local prockallsyms_Fugitive = vim.api.nvim_create_augroup("prockallsyms_Fugitive", {})
 autocmd("BufWinEnter", {
     group = prockallsyms_Fugitive,
@@ -58,24 +92,24 @@ autocmd("BufWinEnter", {
     end,
 })
 
--- buffer remaps
-vim.keymap.set("n", "<Leader>bb", builtin.buffers, { desc = "[B]iultin [B]uffers, enumerates open buffers" })
-vim.keymap.set("n", "<Leader>bc", function() vim.cmd('bd') end, { desc = "[B]uffer [C]lose" })
-vim.keymap.set("n", "<Leader><Leader>", function() vim.cmd('b#') end, { desc = "Swap to last open buffer" })
+-- AI specific remaps
+vim.keymap.set({"n", "v"}, "<Leader>aa", "<cmd>CodeCompanionActions<CR>", { desc = "[A]I [A]ctions" })
+vim.keymap.set({"n", "v"}, "<Leader>at", "<cmd>CodeCompanionChat Toggle<CR>", { desc = "[A]I [T]oggle Chat" })
+vim.keymap.set("v", "<Leader>ai", "<cmd><,'>CodeCompanion<CR>", { desc = "Begin [A]I [I]nline Prompt" })
 
 -- Go filetype-specific keymaps
 autocmd("FileType", {
     pattern = "go",
     callback = function(event)
-	vim.keymap.set("n", "<Leader>gat", ":GoAddTag<CR>", { desc = "[G]o [A]dd JSON [T]ags to the struct I'm in" })
-	vim.keymap.set("n", "<Leader>gfs", ":GoFillStruct<CR>", { desc = "[G]o [F]ill [S]truct" })
-	vim.keymap.set("n", "<Leader>gfm", ":GoFillSwitch<CR>", { desc = "[G]o [F]ill switch ([M]atch statement kinda)" })
-	vim.keymap.set("n", "<Leader>gie", ":GoIfErr<CR>", { desc = "Add a [G]o [I]f [E]rr statement" })
-	vim.keymap.set("n", "<Leader>gr", ":GoRun<CR>", { desc = "[G]o [R]un" })
-	vim.keymap.set("n", "<Leader>gt", ":GoTest<CR>", { desc = "[G]o [T]est" })
-	vim.keymap.set("n", "<Leader>gb", ":GoBuild<CR>", { desc = "[G]o [B]uild" })
-	vim.keymap.set("n", "<Leader>gmi", ":GoModInit<CR>", { desc = "[G]o [M]od [I]nit" })
-	vim.keymap.set("n", "<Leader>gmt", ":GoModTidy<CR>", { desc = "[G]o [M]od [T]idy" })
+	vim.keymap.set("n", "<Leader>gat", "<cmd>GoAddTag<CR>", { desc = "[G]o [A]dd JSON [T]ags to the struct I'm in" })
+	vim.keymap.set("n", "<Leader>gfs", "<cmd>GoFillStruct<CR>", { desc = "[G]o [F]ill [S]truct" })
+	vim.keymap.set("n", "<Leader>gfm", "<cmd>GoFillSwitch<CR>", { desc = "[G]o [F]ill switch ([M]atch statement kinda)" })
+	vim.keymap.set("n", "<Leader>gie", "<cmd>GoIfErr<CR>", { desc = "Add a [G]o [I]f [E]rr statement" })
+	vim.keymap.set("n", "<Leader>gr", "<cmd>GoRun<CR>", { desc = "[G]o [R]un" })
+	vim.keymap.set("n", "<Leader>gt", "<cmd>GoTest<CR>", { desc = "[G]o [T]est" })
+	vim.keymap.set("n", "<Leader>gbd", "<cmd>GoBuild<CR>", { desc = "[G]o [B]uild" })
+	vim.keymap.set("n", "<Leader>gmi", "<cmd>GoModInit<CR>", { desc = "[G]o [M]od [I]nit" })
+	vim.keymap.set("n", "<Leader>gmt", "<cmd>GoModTidy<CR>", { desc = "[G]o [M]od [T]idy" })
     end,
 })
 
